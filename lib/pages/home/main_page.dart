b64115d5e1e5300e6100fc1206994383043ee5cf
madedirga22 <madedirga22@gmail.com>
2023-11-03 13:31:21 +0800

    Main Page

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -1,8 +1,21 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:market/pages/home/chat_page.dart';
+import 'package:market/pages/home/home_page.dart';
+import 'package:market/pages/home/profile_page.dart';
+import 'package:market/pages/home/wishlist_page.dart';
 import 'package:market/shared/theme.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,37 +37,68 @@ class MainPage extends StatelessWidget {
         ),
         child: BottomAppBar(
           shape: const CircularNotchedRectangle(),
-          notchMargin: 10,
+          notchMargin: 6,
           clipBehavior: Clip.antiAlias,
           child: BottomNavigationBar(
             backgroundColor: backgroundColor3,
+            currentIndex: currentIndex,
+            onTap: (value) {
+              print(value);
+              setState(() {
+                currentIndex = value;
+              });
+            },
             type: BottomNavigationBarType.fixed,
             items: [
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/images/icon_home.png',
-                  width: 20,
+                icon: Container(
+                  margin: const EdgeInsets.only(top: 15, bottom: 5),
+                  child: Image.asset(
+                    'assets/images/icon_home.png',
+                    width: 20,
+                    color: currentIndex == 0
+                        ? primaryColor
+                        : const Color(0xff808191),
+                  ),
                 ),
                 label: '',
               ),
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/images/icon_chat.png',
-                  width: 20,
+                icon: Container(
+                  margin: const EdgeInsets.only(top: 15, bottom: 5),
+                  child: Image.asset(
+                    'assets/images/icon_chat.png',
+                    width: 20,
+                    color: currentIndex == 1
+                        ? primaryColor
+                        : const Color(0xff808191),
+                  ),
                 ),
                 label: '',
               ),
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/images/icon_home.png',
-                  width: 20,
+                icon: Container(
+                  margin: const EdgeInsets.only(top: 15, bottom: 5),
+                  child: Image.asset(
+                    'assets/images/icon_home.png',
+                    width: 20,
+                    color: currentIndex == 2
+                        ? primaryColor
+                        : const Color(0xff808191),
+                  ),
                 ),
                 label: '',
               ),
               BottomNavigationBarItem(
-                icon: Image.asset(
-                  'assets/images/icon_profile.png',
-                  width: 20,
+                icon: Container(
+                  margin: const EdgeInsets.only(top: 15, bottom: 5),
+                  child: Image.asset(
+                    'assets/images/icon_profile.png',
+                    width: 20,
+                    color: currentIndex == 3
+                        ? primaryColor
+                        : const Color(0xff808191),
+                  ),
                 ),
                 label: '',
               ),
@@ -64,14 +108,27 @@ class MainPage extends StatelessWidget {
       );
     }
 
+    Widget body() {
+      switch (currentIndex) {
+        case 0:
+          return const HomePage();
+        case 1:
+          return const ChatPage();
+        case 2:
+          return const WishlistPage();
+        case 3:
+          return const ProfilePage();
+        default:
+          return const HomePage();
+      }
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor1,
       floatingActionButton: cartButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: customBottomNav(),
-      body: Center(
-        child: Text('Main Page'),
-      ),
+      body: body(),
     );
   }
 }
