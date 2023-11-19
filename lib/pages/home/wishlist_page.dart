d4151900ed0248afa69ccdd40bac7e5666120e99
madedirga22 <madedirga22@gmail.com>
2023-11-19 13:28:50 +0800

    Wishlist Page - Empty

--- lib/pages/home/wishlist_page.dart
+++ lib/pages/home/wishlist_page.dart
@@ -1,12 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:market/shared/theme.dart';
 
 class WishlistPage extends StatelessWidget {
   const WishlistPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Wishlist Page'),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor4,
+        centerTitle: true,
+        title: Text(
+          'Favorite',
+          style: primaryTextStyle.copyWith(
+            fontSize: 15,
+            fontWeight: semiBold,
+          ),
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget emptyWishlist() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor1,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/image_wishlist.png',
+                width: 60,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'No Favorite',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: semiBold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Find your favorite shoes',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 12,
+                  fontWeight: regular,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 50,
+                margin: const EdgeInsets.only(top: 30),
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 40,
+                    ),
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text(
+                    'Explore',
+                    style: secondaryTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        emptyWishlist(),
+      ],
     );
   }
 }
