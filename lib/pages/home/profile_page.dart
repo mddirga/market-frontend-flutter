796f5757e5a15e16d732060f592379e2e4661730
madedirga22 <madedirga22@gmail.com>
2023-11-21 19:55:28 +0800

    Profile Page - Header

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:market/shared/theme.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Profile Page'),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor4,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.only(
+              left: defaultMargin,
+              top: 18,
+              bottom: 18,
+              right: defaultMargin,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hallo, Guest1234',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 15,
+                          fontWeight: semiBold,
+                        ),
+                      ),
+                      Text(
+                        '@guest123',
+                        style: subtitleTextStyle.copyWith(
+                          fontSize: 12,
+                          fontWeight: regular,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  'assets/images/button_exit.png',
+                  width: 25,
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+      ],
     );
   }
 }
